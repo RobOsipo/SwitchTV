078fd6f2367a1c973e52691bc9ff1653d9c45f35
Robert <osipovitchr@gmail.com>
2022-04-11 15:26:46 -0400

    modal funtionality + history.push

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
+
 const Modal = (props) => {
     return ReactDOM.createPortal(
 
-        <div className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
-                blah blah blah blah 
+        <div onClick={props.onDismiss} className="ui dimmer modals visible active">
+            <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
+                <div className="header">{props.title}</div>
+                <div className="content">{props.content}</div>
+                <div className="actions">
+                    {props.actions}
+                </div>
             </div>
         </div>, //! <--- Notice the comma, the JSX is only the first argument, just like ReactDOM.Render
 
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import Modal from '../Modal.js'
+import history from '../../history.js'
 
 
 const StreamDelete = () => {
+
+const actions = (
+    <>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button">Cancel</button>
+    </>
+)
+
     return (
         <div className="">
-            <Modal />
+            <Modal 
+                title="Delete Stream"
+                content="Are you sure you want to delete this stream?"
+                actions={actions}
+                onDismiss={() => history.push('/')}
+             />
         </div>
     )
 }
